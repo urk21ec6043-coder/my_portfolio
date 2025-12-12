a488b560b1e8e66c68265dbf5fa3d3c763d76058
urk21ec6043-coder <urk21ec6043@gmail.com>
2025-12-12 19:08:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyBMv-aDs1qd9kvBJK5PDIiBs1ZysJMrxAA),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyBMv-aDs1qd9kvBJK5PDIiBs1ZysJMrxAA)
       },
       resolve: {
         alias: {
